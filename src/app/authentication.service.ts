d0d11a0e39104dfce5359b6ac69524de91cf6bf2
Anthony Perez <antrp93@gmail.com>
2020-04-27 16:25:08 -0500

    signup method

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {ApiService} from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  token: any
+  constructor(private apiService: ApiService) { }
+
+ async signup(username,password){
+   return await this.apiService.post('auth/signup', {username, password});
+
+  }
 }
