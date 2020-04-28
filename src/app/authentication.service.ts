0ed1b6a1497573999d1d7bb25e6e4e0bb106ca3c
Anthony Perez <antrp93@gmail.com>
2020-04-27 20:18:40 -0500

    fleshed out authentication service

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,16 +1,32 @@
-import { Injectable } from '@angular/core';
-import {ApiService} from './api.service';
+import { Injectable } from "@angular/core";
+import { ApiService } from "./api.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthenticationService {
+  token: string;
+  constructor(private apiService: ApiService) {}
 
-  token: any
-  constructor(private apiService: ApiService) { }
+  async signup(username, password) {
+    let response = this.apiService.post("auth/signup", { username, password });
+    return await response;
+  }
 
- async signup(username,password){
-   let response = this.apiService.post('auth/signup', {username, password});
-   return await response;
+  async login() {
+    let loginData = {
+      username: "Test User",
+      password: "password",
+    };
+
+    await this.apiService.post("auth/signup", loginData);
+
+    const response = await this.apiService.post("auth/login", loginData);
+
+    this.token = response.token;
+  }
+
+  getToken(){
+    return this.token;
   }
 }
