495b4a2abd096a5ce4f4fc49f74604f8f54d2bb4
Anthony Perez <antrp93@gmail.com>
2020-04-28 19:41:26 -0500

    initial intercceptor fleshed out

--- src/app/auth-interceptor.ts
+++ src/app/auth-interceptor.ts
@@ -1,2 +1,13 @@
-export class AuthInterceptor {
+import {Injectable } from '@angular/core';
+import {HttpClient, HttpRequest, HttpHandler,HttpEvent, HttpInterceptor} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AuthenticationService} from './authentication.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthenticationService){}
+
+    intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>>{
+        return next.handle(request);
+    }
 }
